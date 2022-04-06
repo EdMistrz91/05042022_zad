f9b72a0bb19da9139736547f74bd1c9884928cd2
Kamil Lipiński <kamil.lipinski192@gmail.com>
2022-04-06 19:28:09 +0200

    Dodanie lomboka oraz wykorzystanie konstruktora

--- src/main/java/com/sda/arppl2/methodology/Bear.java
+++ src/main/java/com/sda/arppl2/methodology/Bear.java
@@ -1,5 +1,8 @@
 package com.sda.arppl2.methodology;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 public class Bear {
     String imie;
     int wiek;
--- src/main/java/com/sda/arppl2/methodology/Main.java
+++ src/main/java/com/sda/arppl2/methodology/Main.java
@@ -4,19 +4,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("zadanie 1 zrobione!");
         Bear[] bear = new Bear[4];
-        for (int i = 0; i < 4; i++) {
-            bear[i] = new Bear();
-        }
-        bear[0].imie = "Marian";
-        bear[1].imie = "Olek";
-        bear[2].imie = "Filipek";
-        bear[3].imie = "Teddy";
-
-        bear[0].wiek = 32;
-        bear[1].wiek = 2;
-        bear[2].wiek = 10;
-        bear[3].wiek = 13;
-
+            bear[0] = new Bear("Marian",32);
+            bear[1] = new Bear("Olek",2);
+            bear[2] = new Bear("Filipek",10);
+            bear[3] = new Bear("Teddy",13);
 
     }
 }
